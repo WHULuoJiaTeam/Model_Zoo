fddd7f61dcdfe21d93852b3f917c7d60b4c41725
QingLin Yang <102777023+seasideGargantua@users.noreply.github.com>
2023-01-11 16:39:59 +0800

    Delete Multi_View_Reconstruction/DATASET_NAME directory

--- Multi_View_Reconstruction/DATASET_NAME/GCNet/GCNet_Mulitiprocessing/eval.sh
+++
@@ -1,1 +0,0 @@
-python eval.py --eval_list="list/whu_validation.txt" --crop_h=384 --crop_w=768 --max_disp=160 --dataset_type="whu" --model_path="checkpoint/checkpoint_gcnet_whu-20_8316.ckpt"
--- Multi_View_Reconstruction/DATASET_NAME/GCNet/GCNet_Mulitiprocessing/list/readme.md
+++
@@ -1,3 +0,0 @@
-The structure of the list file:
-
-id left_path right_path disp_path
